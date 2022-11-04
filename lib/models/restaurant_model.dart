6502e6805021921e9aac55cfecffbba43cce9842
abghifareihand <info.abghifareihan@gmail.com>
2022-11-04 10:19:29 +0700

    add model and api service

---
+++ lib/models/restaurant_model.dart
@@ -0,0 +1,67 @@
+import 'package:mini_project_flutter_alterra/utils/config.dart';
+
+class RestaurantModel {
+  String id;
+  String name;
+  String description;
+  String pictureId;
+  String city;
+  double rating;
+  String? review;
+
+  RestaurantModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.pictureId,
+    required this.city,
+    required this.rating,
+    this.review,
+
+  });
+
+  factory RestaurantModel.fromJson(Map<String, dynamic> json) =>
+      RestaurantModel(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+        pictureId: json["pictureId"],
+        city: json["city"],
+        rating: json["rating"].toDouble(),
+        review: json["review"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "description": description,
+        "pictureId": pictureId,
+        "city": city,
+        "rating": rating,
+        "review": review,
+      };
+
+  String getSmallPicture() => Config.imageSmall + pictureId;
+  String getMediumPicture() => Config.imageMedium + pictureId;
+  String getLargePicture() => Config.imageLarge + pictureId;
+}
+
+class CustomerReview {
+  CustomerReview({
+    required this.name,
+    required this.review,
+  });
+
+  String name;
+  String review;
+
+  factory CustomerReview.fromJson(Map<String, dynamic> json) => CustomerReview(
+        name: json["name"],
+        review: json["review"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "review": review,
+      };
+}
